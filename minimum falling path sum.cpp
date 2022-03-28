40d7644bfc45d03807b2e2d624b5a63e83b04d2e
devanshi-code <devu2732@gmail.com>
2022-03-28 23:40:11 +0530

    commit

---
+++ minimum falling path sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+      int m=matrix.size();
+      int n=matrix[0].size();
+    
+      int ans=1000000;
+      for(int currentCol=0;currentCol<n;currentCol++){
+          
+      }
+        
+    }
+};
