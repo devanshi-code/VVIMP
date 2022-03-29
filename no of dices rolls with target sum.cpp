1cd178c7da6801cd45caea9caab4b976cbe65277
devanshi-code <devu2732@gmail.com>
2022-03-29 22:59:36 +0530

    commit

---
+++ no of dices rolls with target sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int totalWays(int n,int f,int tSum)
+    {
+        if(n==0 && tSum==0)
+            return 1;
+        if(n!=0 || tSum<=0)
+            return 0;
+        int ways=0;
+    }    
+    int numRollsToTarget(int n, int k, int target) {
+        return totalWays(d,f,tSum)
+    }
+};
